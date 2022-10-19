227726f38e835bcb50250c53f75f391be9e770ab
santosmota <62900765+santosmota@users.noreply.github.com>
2022-10-19 11:18:24 +0200

    Figure Phases of Frequency Control

--- PythonCharts/Intro_FreqRespPhases.py
+++ PythonCharts/Intro_FreqRespPhases.py
@@ -61,14 +61,14 @@ def plot_charts(figure_type='.pdf'):
                  color='black',
                  linewidth=1,
                  linestyle='dashed',
-                 label=r'Load')
+                 label=r'Electric load (consumption)')
 
     axes[0].plot(csv_df['time'],
                  csv_df['pmec'],
                  color=pe.cor_dalt['gray'],
                  linewidth=1,
                  linestyle='solid',
-                 label=r'Generation')
+                 label=r'Mechanical power (generation)')
 
     axes[1].plot(csv_df['time'],
                  csv_df['f'],
@@ -98,19 +98,26 @@ def plot_charts(figure_type='.pdf'):
                  linestyle='solid',
                  label=r'Secondary')
 
+    # axes[2].plot(csv_df['time'],
+    #              csv_df['pinert'] + csv_df['pprim'] + csv_df['psec'],
+    #              color=pe.cor_dalt['gray'],
+    #              linewidth=1,
+    #              linestyle='dotted',
+    #              label=r'Total reserve')
+
     ##########################################################################
     # axis limits
     ##########################################################################
-    axes[2].set_xticks(np.arange(0, 100, 20))
+    axes[2].set_xticks(np.arange(0, 100, 10))
     axes[2].set_xlim([0, 80])
 
-    axes[0].set_yticks(np.arange(0.50, 0.60, 0.02))
+    axes[0].set_yticks(np.arange(0.50, 0.60, 0.01))
     axes[0].set_ylim([0.499, 0.56])
 
-    axes[1].set_yticks(np.arange(0.98, 1.01, 0.005))
+    axes[1].set_yticks(np.arange(0.98, 1.01, 0.002))
     axes[1].set_ylim([0.9875, 1.0005])
 
-    axes[2].set_yticks(np.arange(0, 0.08, 0.02))
+    axes[2].set_yticks(np.arange(0, 0.08, 0.01))
     axes[2].set_ylim([-0.01, 0.06])
 
     ##########################################################################
@@ -127,12 +134,62 @@ def plot_charts(figure_type='.pdf'):
     ##########################################################################
     # https://matplotlib.org/stable/gallery/color/named_colors.html
     # colors lightgray gray aliceblue whitesmoke
-    # corlegenda = 'whitesmoke'
+    corlegenda = 'whitesmoke'
     #
-    # axes[0][0].annotate(r'a', xy=(0.9, 0.15), xycoords='axes fraction',
-    #                          bbox=dict(boxstyle='circle', fc=corlegenda))
+    axes[0].annotate(r'a', xy=(0.05, 0.25), xycoords='axes fraction',
+                     bbox=dict(boxstyle='circle', fc=corlegenda))
 
+    axes[1].annotate(r'b', xy=(0.05, 0.25), xycoords='axes fraction',
+                     bbox=dict(boxstyle='circle', fc=corlegenda))
 
+    axes[2].annotate(r'c', xy=(0.05, 0.25), xycoords='axes fraction',
+                     bbox=dict(boxstyle='circle', fc=corlegenda))
+
+    ##########################################################################
+    # annotations
+    ##########################################################################
+    axes[0].annotate(r'Nadir happens when powers equalize',
+                     xy=(12.02, 0.55), xycoords='data',
+                     xytext=(15, 0.53), textcoords='data',
+                     ha='left',
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+
+    axes[1].annotate(r'RoCoF',
+                     xy=(10.0, 0.99888), xycoords='data',
+                     xytext=(12.0, 0.995), textcoords='data',
+                     arrowprops=dict(arrowstyle="->",connectionstyle="arc3"))
+
+    axes[1].annotate(r'Nadir',
+                     xy=(12.02, 0.9888), xycoords='data',
+                     xytext=(20, 0.9885), textcoords='data',
+                     va='center',
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+
+    axes[1].annotate(r'Level defined by primary response',
+                     xy=(20.0, 0.99), xycoords='data',
+                     xytext=(15.0, 0.999), textcoords='data',
+                     ha='left',
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+
+    axes[1].annotate(r'Secondary response',
+                     xy=(40.0, 0.995255), xycoords='data',
+                     xytext=(50.0, 0.9950), textcoords='data',
+                     ha='left',
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+
+    axes[2].annotate(r'Proportional response',
+                     xy=(26.0, 0.05), xycoords='data',
+                     xytext=(12.0, 0.0275), textcoords='data',
+                     ha='left',
+                     va='center',
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+
+    axes[2].annotate(r'Integral response',
+                     xy=(45.0, 0.032), xycoords='data',
+                     xytext=(60.0, 0.05), textcoords='data',
+                     ha='right',
+                     va='center',
+                     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
 
     ##########################################################################
     # axis legends
@@ -161,7 +218,7 @@ def main():
     print("Function name: ", main.__name__)
 
     figure_type = '.pdf'
-    #figure_type = ''
+    # figure_type = ''
 
     plot_charts(figure_type=figure_type)
 
